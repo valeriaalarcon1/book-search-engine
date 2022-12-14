12d1333bb5775e00f0c367e93236a93b41fec88c
Valeria <alarconmvaleria@gmail.com>
2022-12-14 16:51:58 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const { typeDefs, resolvers } = require('./schemas');
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,7 +20,7 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 }
 
 app.get('/', (req, res) => {
